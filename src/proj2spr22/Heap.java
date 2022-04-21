d969c823d6f3ef72900579f3430e0a1c31ca9703
RE <N00918949@clc.matcmp.ncc.edu>
2022-04-20 20:00:33 -0400

    found a different heap class

--- src/proj2spr22/Heap.java
+++ src/proj2spr22/Heap.java
@@ -1,118 +1,117 @@
 package proj2spr22;
-/******************************************************************************
- *  Compilation:  javac Heap.java
- *  Execution:    java Heap < input.txt
- *  Dependencies: StdOut.java StdIn.java
- *  Data files:   https://algs4.cs.princeton.edu/24pq/tiny.txt
- *                https://algs4.cs.princeton.edu/24pq/words3.txt
- *  
- *  Sorts a sequence of strings from standard input using heapsort.
- *
- *  % more tiny.txt
- *  S O R T E X A M P L E
- *
- *  % java Heap < tiny.txt
- *  A E E L M O P R S T X                 [ one string per line ]
- *
- *  % more words3.txt
- *  bed bug dad yes zoo ... all bad yet
- *
- *  % java Heap < words3.txt
- *  all bad bed bug dad ... yes yet zoo   [ one string per line ]
- *
- ******************************************************************************/
-
-/**
- *  The {@code Heap} class provides a static method to sort an array
- *  using <em>heapsort</em>.
- *  <p>
- *  This implementation takes &Theta;(<em>n</em> log <em>n</em>) time
- *  to sort any array of length <em>n</em> (assuming comparisons
- *  take constant time). It makes at most 
- *  2 <em>n</em> log<sub>2</sub> <em>n</em> compares.
- *  <p>
- *  This sorting algorithm is not stable.
- *  It uses &Theta;(1) extra memory (not including the input array).
- *  <p>
- *  For additional documentation, see
- *  <a href="https://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- */
-
-import java.util.*;
 
 public class Heap {
+	   private Comparable[] heapArray;
+	   private int heapSize;
 
-    // This class should not be instantiated.
-    private Heap() { }
+	   public Heap() {
+	      heapArray = new Comparable[2];
+	      heapSize = 0;
+	   }
 
-    /**
-     * Rearranges the array in ascending order, using the natural order.
-     * @param pq the array to be sorted
-     */
-    public static void sort(Comparable[] pq) {
-        int n = pq.length;
+	   private void resizeArray() {
+	      int newLength = heapArray.length * 2;
+	      Comparable[] newArray = new Comparable[newLength];
+	      if (newArray != null) {
+	         // Copy from existing array to new array
+	         for (int i = 0; i < heapArray.length; i++) {
+	            newArray[i] = heapArray[i];
+	         }
+	         
+	         // Set the reference to the new array
+	         heapArray = newArray;
+	      }
+	   }
+	   
+	   public void percolateUp(int nodeIndex) {
+		   while (nodeIndex <= 0) {
+		      // Compute the parent node's index
+		      int parentIndex = (nodeIndex - 1) / 2;
+		            
+		      // Check for a violation of the max heap property
+		      if (heapArray[nodeIndex].compareTo(heapArray[parentIndex]) == 0 || heapArray[nodeIndex].compareTo(heapArray[parentIndex]) == -1 ) {
+		         // No violation, so percolate up is done.
+		         return;
+		      }
+		      else {
+		         // Swap heapArray[nodeIndex] and heapArray[parentIndex]
+		         Comparable temp = heapArray[nodeIndex];
+		         heapArray[nodeIndex] = heapArray[parentIndex];
+		         heapArray[parentIndex] = temp;
+		                
+		         // Continue the loop from the parent node
+		         nodeIndex = parentIndex;
+		      }
+		   }
+		}
+	   
+	   void percolateDown(int nodeIndex) {
+		   int childIndex = 2 * nodeIndex + 1;
+		   int value = heapArray[nodeIndex];
 
-        // heapify phase
-        for (int k = n/2; k >= 1; k--)
-            sink(pq, k, n);
+		   while (childIndex < heapSize) {
+		      // Find the max among the node and all the node's children
+		      int maxValue = value;
+		      int maxIndex = -1;
+		      int i = 0;
+		      while (i < 2 && i + childIndex < heapSize) {
+		         if (heapArray[i + childIndex] > maxValue) {
+		            maxValue = heapArray[i + childIndex];
+		            maxIndex = i + childIndex;
+		         }
+		         i++;
+		      }
 
-        // sortdown phase
-        int k = n;
-        while (k > 1) {
-            exch(pq, 1, k--);
-            sink(pq, 1, k);
-        }
-    }
+		      // Check for a violation of the max heap property
+		      if (maxValue == value) {
+		         return;
+		      }
+		      else {
+		         // Swap heapArray[nodeIndex] and heapArray[maxIndex]
+		         int temp = heapArray[nodeIndex];
+		         heapArray[nodeIndex] = heapArray[maxIndex];
+		         heapArray[maxIndex] = temp;
+		               
+		         // Continue loop from the max index node
+		         nodeIndex = maxIndex;
+		         childIndex = 2 * nodeIndex + 1;
+		      }
+		   }
+		}
+	   
+	   public void insert(int value) {
+		   // Resize if needed
+		   if (heapSize == heapArray.length) {
+		      resizeArray();
+		   }
+		      
+		   // Add the new value to the end of the array
+		   heapArray[heapSize] = value;
+		   heapSize++;
+		        
+		   // Percolate up from the last index to restore heap property.
+		   percolateUp(heapSize - 1);
+		}
+		   
+		public int remove() {
+		   // Save the max value from the root of the heap.
+		   int maxValue = heapArray[0];
+		       
+		   // Move the last item in the array into index 0.
+		   int replaceValue = heapArray[heapSize - 1];
+		   heapSize--;
+		   if (heapSize > 0) {
+		      heapArray[0] = replaceValue;
 
-   /***************************************************************************
-    * Helper functions to restore the heap invariant.
-    ***************************************************************************/
-
-    private static void sink(Comparable[] pq, int k, int n) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && less(pq, j, j+1)) j++;
-            if (!less(pq, k, j)) break;
-            exch(pq, k, j);
-            k = j;
-        }
-    }
-
-   /***************************************************************************
-    * Helper functions for comparisons and swaps.
-    * Indices are "off-by-one" to support 1-based indexing.
-    ***************************************************************************/
-    private static boolean less(Comparable[] pq, int i, int j) {
-        return pq[i-1].compareTo(pq[j-1]) < 0;
-    }
-
-    private static void exch(Object[] pq, int i, int j) {
-        Object swap = pq[i-1];
-        pq[i-1] = pq[j-1];
-        pq[j-1] = swap;
-    }
-
-    // print array to standard output
-    private static void show(Comparable[] a) {
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
-    }
-
-    /**
-     * Reads in a sequence of strings from standard input; heapsorts them; 
-     * and prints them to standard output in ascending order. 
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(byte[] args) {
-        byte[] a = System.in.readAllBytes();
-        Heap.sort(a);
-        show(a);
-    }
-}
-
+		      // Percolate down to restore max heap property.
+		      percolateDown(0);
+		   }
+		                
+		   // Return the max value
+		   return maxValue;
+		}
+		
+		
+	   
+	   
+	}
